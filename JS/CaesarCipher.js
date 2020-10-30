a79ea54a24465c0d84b1302d00f9e313017d4678
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-10-30 19:13:46 +0000

    Forced substitution of all characters added

--- JS/CaesarCipher.js
+++ JS/CaesarCipher.js
@@ -1,5 +1,19 @@
 const allShifts = [...Array(25).keys()].map( n => n + 1 )
-const cipher = (str, keys=allShifts) => {
+const cipher = (str, keys=allShifts, force=false) => {
+    if(force)
+    {
+        const chars = str.split("")
+        const res = keys.map( key => {
+            return chars.map( c => {
+                const char = c.charCodeAt(0)
+                const code = char + key
+                return String.fromCharCode(code < 0 ? (256 + code) % 256 : code % 256)
+            })
+        })
+
+        return res.map( (r, i) => { return { key: keys[i] > 0 ? keys[i] : 0 - keys[i] , result: r.join("") } } )
+    }
+
     const chars = str.split("")
     const res = keys.map( key => {
         return chars.map( c => {
@@ -22,7 +36,7 @@ const cipher = (str, keys=allShifts) => {
     return res.map( (r, i) => { return { key: keys[i], result: r.join("") } } )
 }
 
-const decipher = (str, key=allShifts) => {
+const decipher = (str, key=allShifts, force=false) => {
     const keys = key.map( v => 26 - v % 26 )
     return cipher(str, keys)
 }
