15c9c0ed647e3ea24881b81e9e88ef81a52b4607
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-10-31 23:16:23 +0000

    Update VernamCipher.js

--- JS/VernamCipher.js
+++ JS/VernamCipher.js
@@ -1,4 +1,5 @@
 const vernamEncrypt = (str, key) => {
+    // vernam cipher requires key to be same length as str
     if(str.length !== key.length) return "Invalid key"
 
     const chars = str.split("")
@@ -6,16 +7,20 @@ const vernamEncrypt = (str, key) => {
 
     const res = chars.map( (c, i) => {
         const char = c.charCodeAt(0)
+        // get shift from corresponding position at key string 
         const k = keys[i].charCodeAt(0)
 
+        // uppercase
         if(char > 64 && char < 91)
         {
             return String.fromCharCode(65 + (char + k) % 65 % 26)
         }
+        // lowercase
         else if(char > 96 && char < 123)
         {
             return String.fromCharCode(97 + (char + k) % 97 % 26)
         }
+        // keep none alphabets untouched
         else
         {
             return String.fromCharCode(char)
