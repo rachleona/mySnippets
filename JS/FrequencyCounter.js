564823a719899837f8ad2b4ab62e71460d1412df
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-11-06 18:41:24 +0000

    Update FrequencyCounter.js

--- JS/FrequencyCounter.js
+++ JS/FrequencyCounter.js
@@ -1,12 +1,18 @@
 const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+// take in alphabets as argument to allow usage in different languages
 const countAlphabets = (str, a=alphabets) => {
+    // turn to uppercase (if possible)
     const s = str.toUpperCase()
+    // initialise results corresponding to each alphabet in given set
     const res =[...Array(a.length).keys()].map( v => 0 )
+    
     
     s.split("").map( c => {
         const i = a.findIndex( v => v == c )
         if(i >= 0) res[i] += 1
     })
 
+    // return each alphabet and frequency in object format
     return a.reduce( (obj, key, id) => { return { ...obj, [key]: res[id] } }, {} )
 }
