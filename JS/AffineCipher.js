3d37066cd6e13b9af8b42f49764470257d0c62f3
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-12-01 23:35:12 +0000

    Update AffineCipher.js

--- JS/AffineCipher.js
+++ JS/AffineCipher.js
@@ -22,4 +22,30 @@ const encrypt = (plaintext, keys=defaultKeys) => {
     return res.map( (r, i) => { return { key: keys[i], result: r.join("") } } )
 }
 
+const decrypt = (ciphertext, keys=defaultKeys) => {
+    const chars = ciphertext.split("")
+    const res = keys.map(([a, b]) => {
+        const cipheredAlph = [...Array(26).keys()].map( x => {
+            return (x * a + b) % 26
+        })
+
+        return chars.map(c => {
+            const char = c.charCodeAt(0)
+            if(char > 64 && char < 91)
+            {
+                return String.fromCharCode(65 + cipheredAlph.indexOf(char - 65))
+            }
+            else if(char > 96 && char < 123)
+            {
+                return String.fromCharCode(97 + cipheredAlph.indexOf(char - 97))
+            }
+            else
+            {
+                return String.fromCharCode(char)
+            }
+        })
+    })
+
+    return res.map( (r, i) => { return { key: keys[i], result: r.join("") } } )
+}
 
