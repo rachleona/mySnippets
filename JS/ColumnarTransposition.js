92b1d893e40cf2ca166c57e1517b0c53e553c0f3
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-12-09 23:27:04 +0000

    Update ColumnarTransposition.js

--- JS/ColumnarTransposition.js
+++ JS/ColumnarTransposition.js
@@ -1,4 +1,5 @@
 const encrypt = (plaintext, key) => {
+    // arrange key and convert it to interpreted order to swap by e.g "ACB" -> [0, 2, 1]
     const k = key.split("").sort().map( v => key.indexOf(v) )
     const len = k.length
     const chars = plaintext.split("")
@@ -7,24 +8,29 @@ const encrypt = (plaintext, key) => {
 
     for(let i = 0; i < chars.length; i += len)
     {
+        // get substring of key length
         let arr = []
         for(let j = 0; j < len; j++)
         {
+            if(!chars[i + j]) break
             arr.push(chars[i + j])
         }
-
+        
+        // padding if string not long enough
         arr = arr.length == len ? arr : arr.concat(Array(len).fill('X')).slice(0, len) 
-
+        
+        // rearrange 
         let res = [] 
         arr.map( (v, i) => {
             res[k.indexOf(i)] = v
         })
-
+    
         matrix.push(res)
     }
 
     let ciphertext = ""
-
+    
+    // turn matrix into string column by column
     for(let i = 0; i < len; i++)
     {
         matrix.map( row => {
@@ -36,12 +42,15 @@ const encrypt = (plaintext, key) => {
 }
 
 const decrypt = (ciphertext, key) => {
+    // get swap order
     const k = key.split("").sort().map( v => key.indexOf(v) )
     const len = k.length
     const chars = ciphertext.split("")
+    // get number of rows (each row should be length of key)
     const rows = Math.floor(chars.length / len)
     const matrix = Array(rows)
 
+    // convert to matrix by column
     for(let i = 0; i < chars.length; i++)
     {
         let index = i % rows
@@ -49,6 +58,7 @@ const decrypt = (ciphertext, key) => {
         matrix[index].push(chars[i])
     }
     
+    // rearrange
     const plaintext = matrix.map( row => {
         let arr = []
         for(let i = 0; i < len; i++)
